e6759537358240cf38dfa71ac7186218b6690742
Edwin@T480s <edwin.cb.tang@outlook.com>
2019-01-17 12:03:00 +0800

    Revert "Revert "Finish homework2""
    This reverts commit 1aa136c08f096158f687d67a4e23704da3d99e97.

--- Week2/src/Deque.java
+++ Week2/src/Deque.java
@@ -1,42 +1,42 @@
 import java.util.Iterator;
 
 public class Deque<Item> implements Iterable<Item> {
-    private int _size;
-    private Node _head;
-    private Node _tail;
+    private int size;
+    private Node head;
+    private Node tail;
 
     private class Node {
-        private Item _item;
-        private Node _prev;
-        private Node _next;
+        final private Item item;
+        private Node prev;
+        private Node next;
 
         Node(Item item) {
-            this._item = item;
-            this._prev = null;
-            this._next = null;
+            this.item = item;
+            this.prev = null;
+            this.next = null;
         }
 
-        void set_prev(Node prev) {
-            this._prev = prev;
+        void setPrev(Node prev) {
+            this.prev = prev;
         }
 
-        void set_next(Node next) {
-            this._next = next;
+        void setNext(Node next) {
+            this.next = next;
         }
     }
 
     private class Itr implements Iterator<Item> {
-        private int _cur;
-        private Node _cur_node;
+        private int cur;
+        private Node curNode;
 
         Itr() {
-            this._cur = 0;
-            this._cur_node = Deque.this._head;
+            this.cur = 0;
+            this.curNode = Deque.this.head;
         }
 
         @Override
         public boolean hasNext() {
-            return _cur != Deque.this.size();
+            return cur != Deque.this.size();
         }
 
         @Override
@@ -44,10 +44,11 @@ public class Deque<Item> implements Iterable<Item> {
             if (!this.hasNext()) {
                 throw new java.util.NoSuchElementException();
             }
-            if (this._cur++ != 0) {
-                this._cur_node = this._cur_node._next;
+            if (this.cur != 0) {
+                this.curNode = this.curNode.next;
             }
-            return this._cur_node._item;
+            this.cur++;
+            return this.curNode.item;
         }
 
         @Override
@@ -57,99 +58,82 @@ public class Deque<Item> implements Iterable<Item> {
     }
 
     public Deque() {
-        this._size = 0;
-        this._head = null;
-        this._tail = null;
+        this.size = 0;
+        this.head = null;
+        this.tail = null;
     }
 
     public boolean isEmpty() {
-        return this._size== 0;
+        return this.size == 0;
     }
 
     public int size() {
-        return this._size;
+        return this.size;
     }
 
     public void addFirst(Item item) {
         if (item == null) {
             throw new java.lang.IllegalArgumentException();
         }
-        Node n_head = new Node(item);
-        n_head.set_next(this._head);
-        if (this._head != null) {
-            this._head.set_prev(n_head);
+        Node newHead = new Node(item);
+        newHead.setNext(this.head);
+        if (this.head != null) {
+            this.head.setPrev(newHead);
         } else {
-            this._tail = n_head;
+            this.tail = newHead;
         }
-        this._size++;
-        this._head = n_head;
+        this.size++;
+        this.head = newHead;
     }
 
     public void addLast(Item item) {
         if (item == null) {
             throw new java.lang.IllegalArgumentException();
         }
-        Node n_tail = new Node(item);
-        n_tail.set_prev(this._tail);
-        if (this._tail != null) {
-            this._tail.set_next(n_tail);
+        Node newTail = new Node(item);
+        newTail.setPrev(this.tail);
+        if (this.tail != null) {
+            this.tail.setNext(newTail);
         } else {
-             this._head = n_tail;
+             this.head = newTail;
         }
-        this._size++;
-        this._tail = n_tail;
+        this.size++;
+        this.tail = newTail;
     }
 
     public Item removeFirst() {
         if (this.isEmpty()) {
             throw new java.util.NoSuchElementException();
         }
-        Node cur_head = this._head;
-        this._head = cur_head._next;
-        if (this._head != null) {
-            this._head.set_prev(null);
+        Node curHead = this.head;
+        this.head = curHead.next;
+        if (this.head != null) {
+            this.head.setPrev(null);
+        } else {
+            this.tail = null;
         }
-        cur_head.set_next(null);
-        this._size--;
-        return cur_head._item;
+        curHead.setNext(null);
+        this.size--;
+        return curHead.item;
     }
 
     public Item removeLast() {
         if (this.isEmpty()) {
             throw new java.util.NoSuchElementException();
         }
-        Node cur_tail = this._tail;
-        this._tail = cur_tail._prev;
-        if (this._tail != null) {
-            this._tail.set_next(null);
+        Node curTail = this.tail;
+        this.tail = curTail.prev;
+        if (this.tail != null) {
+            this.tail.setNext(null);
+        } else {
+            this.head = null;
         }
-        cur_tail.set_prev(null);
-        this._size--;
-        return cur_tail._item;
+        curTail.setPrev(null);
+        this.size--;
+        return curTail.item;
     }
 
     public Iterator<Item> iterator() {
         return new Itr();
-    }
-
-    public static void main(String[] args) {
-        Deque deque = new Deque();
-        for (int i=0; i!=10; ++i) {
-            if ((i&1) == 0) {
-                deque.addFirst(i);
-            } else {
-                deque.addLast(i);
-            }
-        }
-        Iterator iterator = deque.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next()+" "+iterator.hasNext());
-        }
-        while (!deque.isEmpty()) {
-            System.out.println(deque.removeFirst());
-            if (!deque.isEmpty()) {
-                System.out.println(deque.removeLast());
-            }
-        }
     }
 }
--- Week2/src/Permutation.java
+++ Week2/src/Permutation.java
@@ -1,5 +1,21 @@
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.StdIn;
 
 public class Permutation {
-    public static void main(String[] args) {}
+
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<String>();
+
+        while (true) {
+            try {
+                randomizedQueue.enqueue(StdIn.readString());
+            } catch (java.util.NoSuchElementException e) {
+                break;
+            }
+        }
+        while (k != 0) {
+            System.out.println(randomizedQueue.dequeue());
+            --k;
+        }
+    }
 }
